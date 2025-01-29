6c3710659dd22919108d81517f2dfed3d1f21100
Saksham Vats <saksham.vats@outlook.com>
2025-01-30 00:25:33 +0530

    Implemented Interpolation Search
    added function myInterpolationSearch in mySearches.h and tested it in testing.cpp.

--- mySearches.h
+++ mySearches.h
@@ -37,3 +37,30 @@ int myBinarySearch(const std::vector<int>& arr, int key)
 
 	return -1;
 }
+
+int myInterpolationSearch(const std::vector<int>& arr, int key)
+{
+	int size{ static_cast<int>(arr.size()) };
+
+	int max{ size - 1 };
+	int min{ 0 };
+
+	while (min <= max && key >= arr[min] && key <= arr[max])
+	{
+		int pos{ min + static_cast<int>((key - arr[min]) * (max - min) / (arr[max] - arr[min])) };
+
+		if (pos < min || pos > max)
+			return -1;
+
+		if (key == arr[pos])
+			return pos;
+
+		else if (key > arr[pos])
+			min = pos + 1;
+
+		else
+			max = pos - 1;
+	}
+
+	return -1;
+}
--- testing.cpp
+++ testing.cpp
@@ -3,11 +3,13 @@
 
 int main()
 {
-	std::vector<int> arr{ 0, 15, -1, 5, 99, 5 };
+	std::vector<int> arr{ 0, 15, -1, 5, 99, 50};
 	std::cout << "Linear Search:\n15 was found at index: " << myLinearSearch(arr, 15) << '\n';
 
 	std::sort(arr.begin(), arr.end());
 	std::cout << "Binary Search:\n99 was found at index: " << myBinarySearch(arr, 99) << '\n';
+
+	std::cout << "Interpolation Search:\n50 was found at index: " << myInterpolationSearch(arr, 50) << '\n';
 
 	return 0;
 }
